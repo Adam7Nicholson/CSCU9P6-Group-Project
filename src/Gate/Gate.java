65101f5c4edaa4b9dd105467b550c2888fe92bfc
Nikolay Petrovski <nmp00008@students.sitr.ac.uk>
2022-03-31 13:46:18 +0300

    Updated the last observers.
    Full implementation the following classes:
    
    GateInfoDatabase, GateConsole, Gate, GOC, PublicInfo.

--- src/Gate/Gate.java
+++ src/Gate/Gate.java
@@ -1,11 +1,20 @@
 package Gate;
-
+/*Please put your student ID in so proper accreditation can be given for your work. 
+Ensure it is only your Student ID and *not* your name as marking is done anonymously.
+Please only add your name on this class if you have worked on this class.
+Work can take any form from refactoring to code writing and anything in between, of course
+You should always take credit for your work.*/
 /**
-* @author 2819600
-* @author 2816391
-*/
+ * @author 2819600
+ * @author 2816391
+ * @author
+ * @author
+ * @author
+ * @author
+ */
 
 import Gate.GateInfoDatabase;
+import Management.ManagementRecord;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -20,19 +29,15 @@ public class Gate{
 	}
 
 	private int status = Status.FREE.ordinal();
-	public static int gateStatus; 
 	private int gateNumber;
-	private int mCode;
-	
+	private int mCode = -1;
 	private GateInfoDatabase gateDatabase;
-	
-	//UI-Elements
-	
-    public Gate(int gateNumber)
-    {
-    	this.gateNumber = gateNumber;
-    }
-    
+
+	public Gate(int gateNumber)
+	{
+		this.gateNumber = gateNumber;
+	}
+
 	/**
 	 * Returns the current status code of the aircraft gate
 	 * @return Integer Instance Variable 'status'
@@ -40,52 +45,52 @@ public class Gate{
 	public int getStatus() {
 		return this.status;
 	}
-	
+
 	/*
 	 * Sets the current status of the gate
 	 */
 	public void setStatus(int statusNumber) {
 		status = statusNumber;
-		gateStatus = status;
 	}
 	/**
-	 * 
-	 * @param gateNumber
+	 * Returns the Gate Number.
 	 */
 	public int getGateNumber()
 	{
 		return this.gateNumber;
 	}
+
+
 	/**
-	 * 
+	 * Assigns the ManagementRecord with code - mCode to the gate.
 	 * @param mCode
 	 */
 	public void allocate (int mCode) {
-		
+		this.mCode = mCode;
+		status = Status.RESERVED.ordinal();
 	}
-	
+
 	/**
-	 * 
+	 * Returning the mCode related to this gate.
+	 * @return Int value of the MR code related to this Gate.
+	 */
+	public int getMCode() {
+		return this.mCode;
+	}
+
+	/**
+	 * Sets the status of the gate to OCCUPIED
 	 */
 	public void docked() {
 		status = Status.OCCUPIED.ordinal();
-		gateStatus = status;
 	}
-	
+
 	/**
-	 * 
+	 * Sets the status of the gate to FREE
 	 */
 	public void departed() {
 		status = Status.FREE.ordinal();
-		gateStatus = status;
-	}
-	
-	/**
-	 * 
-	 */
-	public void refresh() {
-		
+		mCode = -1;
 	}
 
-	
 }
--- src/Gate/GateInfoDatabase.java
+++ src/Gate/GateInfoDatabase.java
@@ -1,24 +1,54 @@
 package Gate;
 
 import java.util.List;
+import java.util.Observable;
 import java.util.Vector;
 
+import Gate.Gate.Status;
+import Management.ManagementRecord;
 
+/*Please put your student ID in so proper accreditation can be given for your work.
+Ensure it is only your Student ID and *not* your name as marking is done anonymously.
+Please only add your name on this class if you have worked on this class.
+Work can take any form from refactoring to code writing and anything in between, of course
+You should always take credit for your work.*/
 /**
-* @stereotype model
-* @author 2819600
-* @author 2816391
-*/
+ * @author 2819600
+ * @author 2816391
+ * @author
+ * @author
+ * @author
+ * @author
+ */
 
-public class GateInfoDatabase {
+public class GateInfoDatabase extends Observable{
 	private Vector<Gate> gates = new Vector<Gate>();
-	public int maxGateNumber = 2;
-	
+	public int maxGateNumber = 3;
 
+	/**
+	 * Add a gate to the Database.
+	 * @param g
+	 */
 	public void addGate(Gate g) {
 		gates.add(g);
+		setChanged();
+		notifyObservers();
 	}
-	
+
+	/**
+	 * This method returns the number of gates in the system.
+	 * @return maxGateNumber
+	 */
+	public int getNumberOfGates()
+	{
+		return maxGateNumber;
+	}
+
+	/**
+	 * Returns the status of a selected gate.
+	 * @param gateNumber
+	 * @return Int value of given gate's status, if one exists.
+	 */
 	public int getStatus(int gateNumber) {
 		for(Gate g : this.gates) {
 			if (gateNumber == g.getGateNumber())return g.getStatus();
@@ -26,67 +56,91 @@ public class GateInfoDatabase {
 		return -1;
 	}
 	
+	/**
+	 * Returns the status of a selected gate in String.
+	 * @param gateNumber
+	 * @return String value of given gate's status, if one exists.
+	 */
+	public String getStringStatus(int gateNumber) {
+		return Status.values()[getStatus(gateNumber)].toString();
+	}
+	
+	/**
+	 * Returns the statuses all the gates in the Database.
+	 * @return Int[] value of all the gates' statuses.
+	 */
 	public int[] getStatuses() {
 		int statuses[] = new int[maxGateNumber];
 		int i = 0;
 		for (Gate g : this.gates) {
-				statuses[i] = g.getStatus();
-				i++;
-			}
+			statuses[i] = g.getStatus();
+			i++;
+		}
 		i = 0;
 		return statuses;
 	}
 
-	
+
 	/**
-	 * 
+	 * Returns the Management Record code associated to the gate.
 	 * @param gateNumber
-	 * @param mCode
+	 * @return Int value of the mCode related to the given gate, if it exists.
+	 */
+	public int getMCode(int gateNumber) {
+		for(Gate g : this.gates) {
+			if (gateNumber == g.getGateNumber())return g.getMCode();
+		}
+		return -1;
+	}
+
+	/**
+	 * Makes a connection between a Gate and an Management Record
+	 * by storing mCode into the gate with gateNumber.
+	 * When this is finished observers are notified.
+	 * @param gateNumber, mCode
 	 */
 	public void allocate(int gateNumber, int mCode) {
-		
+		for (Gate g : gates) {
+			if (g.getGateNumber() == gateNumber) {
+				g.allocate(mCode);
+				setChanged();
+				notifyObservers();
+				break;
+			}
+		}
 	}
-	
+
 	/**
-	 * 
+	 * The Gate with gateNumber has its status updated.
+	 * When this is finished observers are notified.
 	 * @param gateNumber
-	 * @param mCode
 	 */
-	public void docked(int gateNumber, int mCode) {
-		
+	public void docked(int gateNumber) {
+		for (Gate g : gates) {
+			if (g.getGateNumber() == gateNumber) {
+				g.docked();
+				setChanged();
+				notifyObservers();
+				break;
+			}
+		}
 	}
-	
+
 	/**
-	 * 
+	 * This method is called when the Management Record no longer is present at the Gate with gateNumber.
+	 * When this is finished observers are notified.
 	 * @param gateNumber
-	 * @param mCode
 	 */
-	public void departed(int gateNumber, int mCode) {
-		
+	public void departed(int gateNumber) {
+		for (Gate g : gates) {
+			if (g.getGateNumber() == gateNumber) {
+				g.departed();
+				setChanged();
+				notifyObservers();
+				break;
+			}
+		}
 	}
-	
-	/*
-	 * GETTERS & SETTERS
-	 */
-	
-	//GETTERS 
-	/**
-	 * 
-	 * @param gateNumber
-	 * @return
-	 */
-	public int getStatus(int gateNumber) {
-		return this.getStatus(gateNumber);
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public int[] getStatuses() {
-		int statuses[] = {};
-		for (Gate g : this.gates) statuses[statuses.length] = g.getStatus();
-		return statuses;
-	}
-	//SETTERS
+
+
 }
